ace4eee63609eb01b05190fb22525c691df9adc0
Ahmad Karambakhsh <ahmad.karambakhsh@gmail.com>
2020-12-23 17:02:06 +0800

    update

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-python detect.py --source ..\Dataset_Zhitang_Yolo5_3\test\images --weights runs\train\exp\weights\best.pt --conf 0.25
+python detect.py --source ..\Dataset_Zhitang_Yolo5_3\test\images --weights runs\train\exp\weights\best.pt --conf 0.25 --save-txt
